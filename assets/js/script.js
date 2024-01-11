b1e3339a0b31ae29fb6b64060b6a3a7298182aff
richie nolan <richienolan23@gmail.com>
2024-01-11 20:45:47 +0000

    add process options function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,26 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+
+    let opAtrray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === 'options') {
+            opAtrray.push(entry[1]);
+        }
+    }
+
+    form.delete('options');
+
+    form.append('options', opAtrray.join());
+
+    return form;
+}
+
 async function postForm(e) {
 
-    const form = new FormData(document.getElementById("checksform"));
+    const form = new processOptions(FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
